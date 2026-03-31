032f3795a4d57d9410c9b6bb79a64087cdb52260
root <root@ip-172-31-77-5.ec2.internal>
2026-03-31 10:32:02 +0000

    message changed

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "DevOps Restart Journey 🚀"
+    return "DevOps Restart Journey progressing🚀"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
