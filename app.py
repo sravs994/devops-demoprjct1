acd085a4f610d4792d3f029da40070fc5e4bcaa2
sravs994 <yash061994@gmail.com>
2026-03-31 15:54:01 +0530

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-#justtesting
-@app.route('/')
-def home():
-    return "DevOps Project Running Successfully!"
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
